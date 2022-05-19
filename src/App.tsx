f30e6236a502df7fd28c785ab49f1e13195c8974
Vampo7152 <umangboss786@gmail.com>
2022-05-19 08:49:43 +0000

    got cli cmds in src

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {mintV2} from './mint';
+import * as web3 from '@solana/web3.js';
 
 function App() {
+  const keypair = '';
+  const env = "devnet";
+  const candyMachine = new web3.PublicKey('GrVSy3ZRbuw5ACbwSEMsj9gULk9MW7QPK1TUYcP6nLM')
+  const rpcUrl = "https://psytrbhymqlkfrhudd.dev.genesysgo.net:8899/";
+
+  async function mintNow(){
+  const tx = await mintV2(keypair, env, candyMachine, rpcUrl);
+
+  console.log('mint_one_token finished', tx);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button onClick={mintNow}>Mint Now!</button>
       </header>
     </div>
   );
